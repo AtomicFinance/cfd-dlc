9103dd18beb8583006d60d17412eb487b308a3a7
Matthew Black <mblackmblackmblack@gmail.com>
2024-04-10 14:34:46 -0400

    --wip-- [skip ci]

--- scripts/build_ios.sh
+++ scripts/build_ios.sh
@@ -2,7 +2,7 @@
 set -e
 cmake -G Xcode -DCMAKE_TOOLCHAIN_FILE=./ios.toolchain.cmake -T buildsystem=1 -DPLATFORM=OS64COMBINED -DCMAKE_BUILD_TYPE=Release -DENABLE_SHARED=OFF -DENABLE_BITCODE=1 -S  . -B build_ios
 
-# cmake -DCMAKE_BUILD_TYPE=Release -DENABLE_SHARED=OFF -S . -B build
+cmake -DCMAKE_BUILD_TYPE=Release -DENABLE_SHARED=OFF -S . -B build
 pushd build
 make -j 4
 popd
